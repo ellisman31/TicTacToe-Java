24ac1abdcce5b4e5bef83af94b5a95eafaf56a93
Ellisman31 <akos99@protonmail.com>
2022-04-28 13:24:19 +0200

    feat(#5): fix regex for input and import GameMap for better accessibility

--- src/com/main/App/Build/App/GameMapSetting.java
+++ src/com/main/App/Build/App/GameMapSetting.java
@@ -9,29 +9,20 @@ import java.util.regex.Pattern;
 
 public class GameMapSetting implements GameMapSettingInterface {
 
-    private String mapIcon = "[ ]";
-
     private int xMapValue;
     private int yMapValue;
     private int currentMapSize;
 
-    private final Scanner input = new Scanner(System.in);
-
     private static final int FIRST_MAP_SIZE_NUM = 0;
     private static final int SECOND_MAP_SIZE_NUM = 1;
-    private static final String REGEX_FOR_INPUT = "[0-9]{0,10}[x][0-9]{0,10}";
+    private static final String REGEX_FOR_INPUT = "[0-9]{1,10}[x][0-9]{1,10}";
     private static final char INPUT_PREFIX = 'x';
     private static final int GET_LAST_LOOP_VALUE = 1;
 
+    private final Scanner input = new Scanner(System.in);
+
     private List<Integer> mapSizeList = new ArrayList<>();
-
-    public String getMapIcon() {
-        return mapIcon;
-    }
-
-    public void setMapIcon(String mapIcon) {
-        this.mapIcon = mapIcon;
-    }
+    private GameMap[][] gameMap;
 
     public void setGameMapSize(){
 
@@ -39,6 +30,7 @@ public class GameMapSetting implements GameMapSettingInterface {
             this.xMapValue = this.mapSizeList.get(FIRST_MAP_SIZE_NUM);
             this.yMapValue = this.mapSizeList.get(SECOND_MAP_SIZE_NUM);
             this.currentMapSize = xMapValue * yMapValue;
+            this.gameMap = new GameMap[xMapValue][yMapValue];
         }
 
     }
@@ -48,8 +40,7 @@ public class GameMapSetting implements GameMapSettingInterface {
         String mapSize;
 
         while (true) {
-            System.out.println("Please provide the game map size (3x3, 4x5..): ");
-            String setGameMapSize = input.nextLine();
+            String setGameMapSize = askForMapSize();
             if(Pattern.matches(REGEX_FOR_INPUT,setGameMapSize)) {
                 mapSize = setGameMapSize;
                 break;
@@ -58,6 +49,11 @@ public class GameMapSetting implements GameMapSettingInterface {
 
         setMapSize(mapSize);
         return true;
+    }
+
+    private String askForMapSize() {
+        System.out.println("Please provide the game map size (3x3, 4x5..): ");
+        return input.nextLine();
     }
 
     private void setMapSize(String mapSize) {
@@ -81,15 +77,19 @@ public class GameMapSetting implements GameMapSettingInterface {
         }
     }
 
-    public int getCurrentMapSize() {
-        return currentMapSize;
-    }
-
     public int getxMapValue() {
         return xMapValue;
     }
 
     public int getyMapValue() {
         return yMapValue;
+    }
+
+    public GameMap[][] getGameMap() {
+        return gameMap;
+    }
+
+    public int getCurrentMapSize() {
+        return currentMapSize;
     }
 }
