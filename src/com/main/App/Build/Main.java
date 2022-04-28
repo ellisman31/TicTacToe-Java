a71532903f1e1f71dfdb26653997fbff8e432f83
Ellisman31 <akos99@protonmail.com>
2022-04-28 10:35:10 +0200

    setup: default class for running the application

---
+++ src/com/main/App/Build/Main.java
@@ -0,0 +1,12 @@
+package com.main.App.Build;
+
+import com.main.App.Build.App.Game;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.gameStarter();
+    }
+
+}
