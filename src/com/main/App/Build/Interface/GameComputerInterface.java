d9554a732c3016162807450c2886b825dac7e7ce
Ellisman31 <akos99@protonmail.com>
2022-05-04 23:34:57 +0200

    refact(#8): remove unused imports

--- src/com/main/App/Build/Interface/GameComputerInterface.java
+++ src/com/main/App/Build/Interface/GameComputerInterface.java
@@ -1,12 +1,10 @@
 package com.main.App.Build.Interface;
 
-import java.util.Set;
-
 public interface GameComputerInterface {
 
-    void randomPlacement(Set<String> playerPlacement);
+    void randomPlacement();
 
-    private void randomPlacementValues(Set<String> playerPlacement) {
+    private void randomPlacementValues() {
     }
 
 }
