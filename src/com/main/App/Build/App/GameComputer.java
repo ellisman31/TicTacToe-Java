b49a4f3e08cf54df657658dc4d8f46863817fe51
Ellisman31 <akos99@protonmail.com>
2022-05-04 23:34:03 +0200

    feat(#9): fix random placement and avoid duplication for #1

--- src/com/main/App/Build/App/GameComputer.java
+++ src/com/main/App/Build/App/GameComputer.java
@@ -1,57 +1,47 @@
 package com.main.App.Build.App;
 
+import com.main.App.Build.BaseModel.GameBase;
 import com.main.App.Build.Interface.GameComputerInterface;
 
-import java.util.HashSet;
 import java.util.Random;
-import java.util.Set;
 
-//TODO: Program the AI and replace placements with the decent icon
-public class GameComputer implements GameComputerInterface {
+public class GameComputer extends GameBase implements GameComputerInterface {
 
-    private final GameSetting gameSetting;
-    private final GameMapDisplay gameMap;
-    private final GameInput gameInput;
     private final Random random = new Random();
-
-    private final Set<String> ownPlacement = new HashSet<>();
 
     private String placement;
 
     public GameComputer(GameSetting gameSetting, GameMapDisplay gameMap, GameInput gameInput) {
-        this.gameSetting = gameSetting;
-        this.gameMap = gameMap;
-        this.gameInput = gameInput;
+        super(gameSetting, gameMap.getGameMap(), gameInput, gameSetting.getOwnPlacement());
     }
 
-    public void randomPlacement(Set<String> playerPlacement) {
-        randomPlacementValues(playerPlacement);
-        if (gameInput.checkPlacementInputIsValid(placement)) {
-            ownPlacement.add(placement);
-            gameMap.getGameMap()[gameInput.getFirstPlacement()][gameInput.getSecondPlacement()].setMapIcon(gameSetting.getSecondPlayerIcon());
+    public void randomPlacement() {
+        randomPlacementValues();
+        if (getGameInput().checkPlacementInputIsValid(this.placement)) {
+            super.getOwnPlacement().add(this.placement);
+            getGameMap()[getGameInput().getFirstPlacement()][getGameInput().getSecondPlacement()]
+                    .setMapIcon(getGameSetting().getSecondPlayerIcon());
+            System.out.println("The computer placed on" + " " + this.placement.toUpperCase());
         }
     }
 
-    private void randomPlacementValues(Set<String> playerPlacement) {
+    private void randomPlacementValues() {
 
         int randomPlacementValue;
         char randomPlacementLetter;
 
-        while(true) {
+        while (true) {
 
-            randomPlacementValue = random.nextInt(gameSetting.getxMapValue()) + 1;
-            randomPlacementLetter = (char) ((char) 97 + random.nextInt(gameSetting.getyMapValue()-1));
+            randomPlacementValue = random.nextInt(getGameSetting().getyMapValue()) + 1;
+            randomPlacementLetter = (char) (97 + random.nextInt(getGameSetting().getxMapValue()));
 
-            if (randomPlacementValue <= gameSetting.getxMapValue()) {
-                this.placement = randomPlacementLetter +""+ randomPlacementValue;
-                if (!playerPlacement.contains(placement) && !ownPlacement.contains(placement)) {
+            if (randomPlacementValue <= getGameSetting().getyMapValue()) {
+                this.placement = randomPlacementLetter + "" + randomPlacementValue;
+                if (!getOwnPlacement().contains(placement) && !getOwnPlacement().contains(this.placement)) {
                     break;
                 }
             }
         }
     }
 
-    public Set<String> getOwnPlacement() {
-        return ownPlacement;
-    }
 }
